bf353ee1a7e7413137e4ec1432839967d21adc28
Markus Rudolph <markhoernchen@gmx.de>
2021-10-15 18:31:10 +0200

    Made some plan

--- lib/meta/plopp.ex
+++ lib/meta/plopp.ex
@@ -1,50 +1,89 @@
 defmodule Meta.TypeSystem do
-  defmodule TypeNameAlreadyExists do
-    defexception [:message]
+  defmodule Errors do
+    defmodule TypeNameAlreadyExists do
+      defexception [:message]
 
-    @impl true
-    def exception(value) do
-      msg = "A type named '#{inspect(value)}' already exists!"
-      %__MODULE__{message: msg}
+      @impl true
+      def exception(value) do
+        msg = "A type named '#{inspect(value)}' already exists!"
+        %__MODULE__{message: msg}
+      end
     end
   end
 
   def new() do
     %{
-      :types => %{
-        #{:name} -> ???
-      },
-      :props => %{
-        #{:type_name, :property_name} -> %{
+      :sequence_type_id => 0,
+      :nodes => %{
+        #{:type, type_id} -> %{
+        #  :name? => name
+        #  :tags => type_tags
+        #}
+        #{:prop,  type_id, property_name} -> %{
         #  :tags => property_tags...,
         #  :compute? => fn/3(type, property, instance) or list_of_commands
-        #  :type => type
+        #}
+        #{:param, type_id, name} -> %{
+        #
         #}
 
+        #type_tags = [
+        # :abstract, :iface
+        #]
+
         #property_tags = [
-        # :nullable, :public
+        # :nullable,
+        # :private,
+        # :static
         #]
       },
       :deps => %{
-        #{:from_name, :to_name, :dep_type} -> data
-        # dep_type # data
-        # :base  -> :to extends :from
-        # :iface -> :to implements :from
+        #{:dep_type, :from, :to} -> data
+        # dep_type      -> roles                  -> data
+        # :extends      -> :from extends     :to    ->
+        #                  :type ->          :type  ->
+        #                  :param ->         :type  ->
+        #                  :param ->         :param ->
+        # :impls        -> :from implements  :to    ->
+        #                  :type ->          :type  ->
+        #                  :param ->         :type  ->
+        #                  :param ->         :param ->
+        # :has_prop     -> :from contains    :to    ->
+        #                  :type ->          :prop  ->
+        # :has_param    -> :from has         :to    ->
+        #                  :type ->          :param ->
+        # :specialize   -> :from specializes :to    ->
+        #                  :type ->          :type  -> Map<:name, type|param>
       }
     }
   end
 
-  def new_type(type_system, type_name)
-    when is_atom(type_name) do
-      types = type_system.types
-      case Map.fetch(types, type_name)  do
-        {:ok, _} -> raise TypeNameAlreadyExists, type_name
-        _ ->
-          type = %{
-            :props => %{},
-          }
-          new_types = Map.put(type_system, type_name, type)
-          Map.put(type_system, :types, new_types)
+  defmodule Builders do
+    defmodule Nodes do
+      def type_id(id)
+        when is_number(id) do
+        {:type, id}
       end
+
+      def type_meta(name? \\ nil, tags \\ [])
+        when is_atom(name?) or is_nil(name?)
+        when is_list(tags) do
+        %{
+          :name => name?,
+          :tags => tags
+        }
+      end
+    end
+  end
+
+  def new_type(type_system) do
+    new_id = type_system.sequence_type_id + 1
+    new_type_id = Builders.Nodes.type_id(new_id)
+    new_type_meta = Builders.Nodes.type_meta()
+    new_system = Map.merge(type_system, %{
+      :sequence_type_id => new_id,
+      :nodes => Map.put(type_system.nodes, new_type_id, new_type_meta)
+    })
+    {:ok, new_system, new_id}
   end
 end
--- test/meta/plopp_test.exs
+++ test/meta/plopp_test.exs
@@ -2,17 +2,10 @@ defmodule Meta.TypeSystemTest do
   use ExUnit.Case
   doctest Meta.TypeSystem
 
-  test "TypeSystem.new_type/2 creates type if name not known" do
+  test "TypeSystem.new_type/2 creates new type" do
     system = Meta.TypeSystem.new()
-    new_system = Meta.TypeSystem.new_type(system, :array)
-    assert %{:types  => %{ :array => _type }} = new_system
-  end
-
-  test "TypeSystem.new_type/2 raises error if type name is known" do
-    empty_system = Meta.TypeSystem.new()
-    system = Meta.TypeSystem.new_type(empty_system, :array)
-    assert_raise Meta.TypeSystem.TypeNameAlreadyExists, fn ->
-      Meta.TypeSystem.new_type(system, :array)
-    end
+    {:ok, result1, id1} = Meta.TypeSystem.new_type(system)
+    {:ok, _, id2} = Meta.TypeSystem.new_type(result1)
+    assert id1 != id2
   end
 end
