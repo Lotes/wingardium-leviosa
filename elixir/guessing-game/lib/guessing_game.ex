327fdf4cb4e039d244681213eab7168b46b31237
Markus Rudolph <markhoernchen@gmx.de>
2021-10-17 20:43:50 +0200

    Solved

--- elixir/guessing-game/lib/guessing_game.ex
+++ elixir/guessing-game/lib/guessing_game.ex
@@ -1,5 +1,15 @@
 defmodule GuessingGame do
-  def compare(secret_number, guess) do
-    # Please implement the compare/2 function
-  end
+  def compare(secret_number), do: "Make a guess"
+  def compare(secret_number, :no_guess), do: "Make a guess"
+  def compare(secret_number, guess)
+    when not is_integer(secret_number)
+    or not is_integer(guess), do: raise "Invalid input"
+  def compare(secret_number, guess)
+    when abs(secret_number -  guess) == 1, do: "So close"
+  def compare(secret_number, guess)
+    when secret_number <  guess, do: "Too high"
+  def compare(secret_number, guess)
+    when secret_number >  guess, do: "Too low"
+  def compare(secret_number, guess)
+    when secret_number == guess, do: "Correct"
 end
