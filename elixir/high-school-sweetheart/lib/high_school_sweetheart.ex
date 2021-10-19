c77fd05229bf1b37e62508cf609988b08744ae25
Markus Rudolph <markhoernchen@gmx.de>
2021-10-19 20:41:59 +0200

    Solved

--- elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
+++ elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -1,32 +1,42 @@
 defmodule HighSchoolSweetheart do
   def first_letter(name) do
-    # Please implement the first_letter/1 function
+    name
+    |> String.trim()
+    |> String.at(0)
   end
 
   def initial(name) do
-    # Please implement the initial/1 function
+    init = name
+    |> first_letter()
+    |> String.upcase()
+    init <> "."
   end
 
   def initials(full_name) do
-    # Please implement the initials/1 function
+    full_name
+    |> String.trim()
+    |> String.split()
+    |> Enum.map(&initial/1)
+    |> Enum.join(" ")
   end
 
   def pair(full_name1, full_name2) do
-    #      ******       ******
-    #    **      **   **      **
-    #  **         ** **         **
-    # **            *            **
-    # **                         **
-    # **     X. X.  +  X. X.     **
-    #  **                       **
-    #    **                   **
-    #      **               **
-    #        **           **
-    #          **       **
-    #            **   **
-    #              ***
-    #               *
-
-    # Please implement the pair/2 function
+    lhs = initials(full_name1)
+    rhs = initials(full_name2)
+    nl = "\n"
+    "     ******       ******" <> nl <>
+    "   **      **   **      **" <> nl <>
+    " **         ** **         **" <> nl <>
+    "**            *            **" <> nl <>
+    "**                         **" <> nl <>
+    "**     "<>lhs<>"  +  "<>rhs<>"     **" <> nl <>
+    " **                       **" <> nl <>
+    "   **                   **" <> nl <>
+    "     **               **" <> nl <>
+    "       **           **" <> nl <>
+    "         **       **" <> nl <>
+    "           **   **" <> nl <>
+    "             ***" <> nl <>
+    "              *" <> nl
   end
 end
