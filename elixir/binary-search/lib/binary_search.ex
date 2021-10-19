23b67c57772b8d96e2cd9640325b705e37f40612
Markus Rudolph <markhoernchen@gmx.de>
2021-10-19 19:50:45 +0200

    Solved

--- elixir/binary-search/lib/binary_search.ex
+++ elixir/binary-search/lib/binary_search.ex
@@ -18,6 +18,33 @@ defmodule BinarySearch do
   """
 
   @spec search(tuple, integer) :: {:ok, integer} | :not_found
+  def search({}, _key), do: :not_found
   def search(numbers, key) do
+    length = Tuple.to_list(numbers)
+      |> Enum.count()
+    [result] = {0, length-1}
+      |> Stream.iterate(fn x -> step(x, key, numbers) end)
+      |> Stream.drop_while(fn x -> not is_atom(x) and not (is_tuple(x) and elem(x, 0) === :ok) end)
+      |> Stream.take(1)
+      |> Enum.to_list()
+    result
   end
+
+  def step({:ok, _index}, _key, _tuple), do: nil
+  def step({lhs, rhs}, key, tuple) do
+    left = elem(tuple, lhs)
+    right = elem(tuple, rhs)
+    cond do
+      lhs === rhs and key === left -> {:ok, lhs}
+      key < left or right < key    -> :not_found
+      true ->
+        m = div(lhs+rhs, 2)
+        middle = elem(tuple, m)
+        cond do
+          left <= key and key <= middle -> {lhs, m}
+          middle < key and key <= right -> {m+1, rhs}
+        end
+    end
+  end
+  def step(atom, _key, _tuple) when is_atom(atom) or is_nil(atom), do: nil
 end
